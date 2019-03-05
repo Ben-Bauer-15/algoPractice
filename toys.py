b4689ab7bf32f379a0fb86e90ee613a8d0d3129e
Ben Bauer <benjaminbauer15@gmail.com>
2019-03-04 19:54:22 -0700

    practice 3/4/19

--- toys.py
+++ toys.py
@@ -2,22 +2,6 @@ def maximumToys(prices, k):
 
     prices.sort()
 
-    # new = []
-
-    # for price in prices:
-    #     if price < k:
-    #         new.append(price)
-    
-    # for i in range(len(new)):
-    #     swaps = False
-    #     for j in range(len(new) - 1):
-    #         if new[j] > new[j + 1]:
-    #             swaps = True
-    #             new[j], new[j + 1] = new[j + 1], new[j]
-
-    #     if not swaps:
-    #         break
-
     items = 0
     total = 0
     for price in prices:
